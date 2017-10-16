610198630b8615a08fa9fdff50ddafbcb0eee115
nimakazemi <kazemy.nima1995@gmail.com>
2017-10-16 09:14:02 +0330

    nima-commit

---
+++ TISS/Pages/Home.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/Main.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="TISS.Pages.Home" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+</asp:Content>
+
--- TISS/Pages/Home.aspx.designer.cs
+++ TISS/Pages/Home.aspx.designer.cs
@@ -7,20 +7,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TISS
+namespace TISS.Pages
 {
 
 
-    public partial class Index
+    public partial class Home
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
