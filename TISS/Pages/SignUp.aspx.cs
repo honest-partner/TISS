9505a750dc251a9ccaaf9fb5b5e949b2dc1a2b8b
honest-partner <honest.partner.1993@gmail.com>
2017-10-18 15:45:31 +0330

    hosein 7/26 3:45

---
+++ TISS/Pages/SignUp.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TISS.UserControls;
+
+namespace TISS.Pages
+{
+    public partial class SignUp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ddlUserType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pnlSignUp.Controls.Add(Page.LoadControl("~/UserControls/ucStudentSignUp.ascx"));
+        }
+    }
+}
